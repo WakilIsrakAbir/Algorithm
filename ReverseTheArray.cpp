d7829cf94b1dbca820ec7c8f4d39995851aae49c
WakilIsrakAbir <wakilisrakabir@gmail.com>
2025-08-10 15:53:28 +0600

    Add files via upload

---
+++ ReverseTheArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int arr[] = {1, 2, 3, 4, 5};
+int n = 5;
+for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-1-i]);
+    }
+cout<<"Reversed Array:";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+cout<<endl;
+return 0;
+}
