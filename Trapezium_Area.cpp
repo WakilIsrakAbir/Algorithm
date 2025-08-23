f14e83a7f0d6f8342414091131fb2f5d8fd8ef03
WakilIsrakAbir <wakilisrakabir@gmail.com>
2025-08-23 18:30:46 +0600

    Add files via upload

---
+++ Trapezium_Area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a,b,h;
+    cout<<"Enter a, b, h: ";
+    cin>>a>>b>> h;
+
+    float area=(a+b)*h/2;
+    cout<<"Area="<<area<<endl;
+}
